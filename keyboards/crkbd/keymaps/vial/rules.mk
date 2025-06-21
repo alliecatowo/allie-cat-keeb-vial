8bb7b54ed2d68f7574883d286528a2666697ad52
Allison <allison@parseceducation.com>
2025-06-21 11:34:35 -0700

    feat: Implement vial support for crkbd 42 key board

---
+++ keyboards/crkbd/keymaps/vial/rules.mk
@@ -0,0 +1,5 @@
+VIA_ENABLE      = yes    # Enable VIA
+VIAL_ENABLE     = yes    # Enable VIAL
+OLED_ENABLE     = yes    # Enable OLED
+MOUSEKEY_ENABLE = yes    # Enable MOUSEKEY
+EXTRAKEY_ENABLE = yes    # Enable EXTRAKEY
