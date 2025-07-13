609bef0e0d61178c77dee1a3b838ffea576a029b
Allison Coleman <98134425+alliecatowo@users.noreply.github.com>
2025-07-13 10:20:31 -0700

    feat: add vial keymaps for sweeq and span

---
+++ keyboards/holykeebs/spankbd/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// Vial configuration
+#define VIAL_KEYBOARD_UID {0x5B, 0x0F, 0x75, 0x91, 0xE9, 0xD6, 0xB3, 0xBC}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 7 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 4 }
+#define DYNAMIC_KEYMAP_LAYER_COUNT 10
---
+++ keyboards/holykeebs/sweeq/keymaps/vial/config.h
@@ -0,0 +1,13 @@
+// Copyright 2023 Idan Kamara (@idank)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+// Good defaults for home row modifiers
+#define TAPPING_TERM 230
+
+// Vial configuration
+#define VIAL_KEYBOARD_UID {0x09, 0x7C, 0x15, 0xD8, 0xF9, 0xC8, 0x9F, 0x29}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 7 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 4 }
+#define DYNAMIC_KEYMAP_LAYER_COUNT 10
